f57fe98fb8f07e08f0da23833d33c49f5edcbe80
Dmitriy Silaev <s91097221699@gmail.com>
2023-08-27 19:58:09 +0300

    Changes from 08/27/2023 19:50 Corrected remarks PEP 8

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -2,10 +2,12 @@ import configuration
 import requests
 import data
 
+
 def post_new_user(body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
-                        json=body,
+                         json=body,
                          headers=data.header_user)
+
 
 response = post_new_user(data.user_body)
 
@@ -24,8 +26,8 @@ def post_new_client_kit(body):
                          json=body,
                          headers=auth_token)
 
+
 response = post_new_client_kit(data.kit_body)
 
 print(response.status_code)
 print(response.json())
-
